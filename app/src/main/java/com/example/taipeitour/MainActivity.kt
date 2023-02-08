56ee540a7ab440b1fc8d677f481cd884e928610b
Wade <tony91097@gmail.com>
2023-02-08 18:09:53 +0800

    import navigation import dataBinding import retrofit feat top bar

--- app/src/main/java/com/example/taipeitour/MainActivity.kt
+++ app/src/main/java/com/example/taipeitour/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.taipeitour
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.taipeitour.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        binding.topAppBar.setNavigationOnClickListener {
+            binding.topAppBar.navigationIcon = null
+        }
+
     }
+
 }
